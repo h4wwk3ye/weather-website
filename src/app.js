62a6d4980ea53254601abe86c419ee112dcbfa26
Farhan Tahir <farhantahir3@gmail.com>
2019-10-30 10:07:29 +0530

    Fixing for Heroku

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000;
 
 // Paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
